32dcb2fd4063ba7b7d5196f4783549355d97bbf6
aditya9546 <singh725589@gmail.com>
2026-02-02 19:48:36 +0530

    Add yellow header box with navigation

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import './Header.css'
 export default function Header() {
   return (
-    <div>Header</div>
+    <header className="header">
+      <div className="header-container">
+        {/* Main Page Title */}
+        <div className="header-title">My React App</div>
+        
+        {/* Navigation Box */}
+        <nav className="nav-box">
+          <button className="nav-btn home-btn">🏠 Home</button>
+          <button className="nav-btn settings-btn">⚙️ Settings</button>
+        </nav>
+      </div>
+    </header>
   )
 }
-
